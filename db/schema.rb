116dde6d62622f67b77db1834757f200d7ff4b96
kashio-tech <gumgum.yk.29@gmail.com>
2020-01-23 23:50:10 +0900

    位置情報追加機能完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_080505) do
+ActiveRecord::Schema.define(version: 2020_01_22_095341) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 2020_01_21_080505) do
     t.float "latitude"
     t.float "longitude"
     t.datetime "taken_at"
-    t.float "speed"
+    t.string "speed"
     t.string "f_number"
     t.string "iso_speed"
     t.string "white_balance"
