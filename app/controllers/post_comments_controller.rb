11ce9e8e5b32db44111e56d056f9f3fd4c6caf5a
kashio-tech <gumgum.yk.29@gmail.com>
2020-01-23 23:50:10 +0900

    位置情報追加機能完成

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,10 +1,20 @@
 class PostCommentsController < ApplicationController
-		def create
+	def create
 		photo = Photo.find(params[:photo_id])
 		comment = current_user.post_comments.new(post_comment_params)
 		comment.photo_id = photo.id
 		comment.save
 		redirect_to	photo_path(photo.id)
+	end
+	def destroy
+		photo = Photo.find(params[:photo_id])
+        if photo.user_id != current_user.id
+            comment = current_user.comments.find_by(photo_id: photo.id, id: comment.id)
+            comment.destroy
+            redirect_back(fallback_location: root_path)
+        else
+            redirect_back(fallback_location: root_path)
+        end
 	end
 
 	private
