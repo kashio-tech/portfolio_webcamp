673a36dcaa3872d5505a67e8f3d6e06c7d1dd241
kashio-tech <gumgum.yk.29@gmail.com>
2020-02-06 16:47:09 +0900

    git pull

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,20 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    devise  :database_authenticatable, :registerable,
+            :recoverable, :rememberable, :validatable
 
-         attachment :profile_image
+    attachment :profile_image
 
-         has_many	:photos, dependent: :destroy
-         has_many	:cameras, dependent: :destroy
-         has_many	:lenses, dependent: :destroy
-         has_many	:post_comments, dependent: :destroy
-         has_many	:favorites, dependent: :destroy
-         has_many :cameras, dependent: :destroy
-         has_many :lenses, dependent: :destroy
+    has_many	:photos
+    has_many	:cameras, dependent: :destroy
+    has_many	:lenses, dependent: :destroy
+    has_many	:post_comments, dependent: :destroy
+    has_many	:favorites, dependent: :destroy
 
-         validates  :last_name, presence: true
-         validates  :first_name, presence: true
-         validates  :last_name_kana, presence: true
-         validates  :first_name_kana, presence: true
-         validates  :display_name, presence: true
+    validates   :last_name, presence: true
+    validates   :first_name, presence: true
+    validates   :last_name_kana, presence: true
+    validates   :first_name_kana, presence: true
+    validates   :display_name, presence: true
 end
