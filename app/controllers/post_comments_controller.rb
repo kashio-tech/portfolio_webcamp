b5e5e9414f0dbd685d6e6315e5d2e9d877aba76c
kashio-tech <gumgum.yk.29@gmail.com>
2020-03-04 20:41:31 +0900

    complete_comment_ajax_create

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,16 @@
 class PostCommentsController < ApplicationController
 	before_action   :authenticate_user!
 	def create
-		photo = Photo.find(params[:photo_id])
-		comment = current_user.post_comments.new(post_comment_params)
-		comment.photo_id = photo.id
-		comment.save
+		@photo = Photo.find(params[:photo_id])
+		@comment = current_user.post_comments.new(post_comment_params)
+		@comment.photo_id = @photo.id
+		@comment.save
 		#redirect_to	photo_path(photo.id)
 	end
 	def destroy
-		photo = Photo.find(params[:photo_id])
-        comment = current_user.post_comments.find(params[:id])
-        comment.destroy
+		@photo = Photo.find(params[:photo_id])
+        @comment = current_user.post_comments.find(params[:id])
+        @comment.destroy
         redirect_back(fallback_location: root_path)
 	end
 
