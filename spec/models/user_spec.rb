4b01823b6165179c139d41c7ce40255e0a336908
kashio-tech <gumgum.yk.29@gmail.com>
2020-03-13 22:56:06 +0900

    rspec5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,3 +3,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+
+Rspec.describe Photo, "モデルに関するテスト", type: :model do
+	describe 'アソシエーション' do
+		it "photoモデルを多数持っている" do
+     is_expected.to have_many(:photos)
+    end
+  end
+end
